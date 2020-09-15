256442e02cd7c14f363301cbc43877bdc43ec787
royeradames <royeraadames@gmail.com>
2020-09-15 14:07:04 -0400

    class is over

--- users/users-model.js
+++ users/users-model.js
@@ -1,30 +1,34 @@
-const db = require("../database/connection.js");
+const db = require("../database/connection.js")
 
 module.exports = {
   add,
   find,
   findBy,
   findById,
-};
+}
 
 function find() {
-  return db("users").select("id", "username").orderBy("id");
+  return db("users").select("id", "username").orderBy("id")
 }
 
 function findBy(filter) {
-  return db("users").where(filter).orderBy("id");
+  return db("users as u")
+  .where(filter)
+  .orderBy("u.id")
+  .join('roles as r', 'r.id', 'u.role')
+  .select('u.id', 'u.username', 'u.password', 'r.name as role')
 }
 
 async function add(user) {
   try {
-    const [id] = await db("users").insert(user, "id");
+    const [id] = await db("users").insert(user, "id")
 
-    return findById(id);
+    return findById(id)
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
 function findById(id) {
-  return db("users").where({ id }).first();
+  return db("users").where({ id }).first()
 }
